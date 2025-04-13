105c953e02eb2e666c6b4ac2673a249dfe4ec915
Karthik <152290686+lkarthik16@users.noreply.github.com>
2025-04-13 23:35:34 +0530

    Update settings.py

--- employment_form/settings.py
+++ employment_form/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://employment-form-0z4x.onrender.com/']
+ALLOWED_HOSTS = ['employment-form-0z4x.onrender.com']
 
 # Application definition
 
